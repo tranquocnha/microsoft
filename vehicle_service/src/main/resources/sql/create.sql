3d37c95653a2fb93dcbfe8e157e0de7287ea518b
tranquocnha <tranquocnha180800@gmail.com>
2023-05-05 11:51:09 +0700

    update

--- vehicle_service/src/main/resources/sql/create.sql
+++ vehicle_service/src/main/resources/sql/create.sql
@@ -1,77 +1,77 @@
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(1, 'tesla', 45, 'blues', 'This is a car of Japan', 'Solar cars', '', 'Shizuoka', 'Sora', 300000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(2, 'Lada', 45, 'blues', 'This is a car of Russia', 'Petrol engines', '', 'Samara Oblast', 'AvtoVAZ', 450000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(3, 'Noble', 45, 'blues', 'This is a car of United Kingdom', 'Petrol engines', '', 'England', 'Noble Automotive Ltd', 34500000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(4, 'Volvo', 45, 'blues', 'This is a car of Sweden', 'Solar cars', '', 'Gothenburg', 'Polestar ', 398000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(5, ' MG', 7, 'lightgrey', 'This is a car of United Kingdom', 'Petrol engines', '', 'Birmingham', 'MG', 123000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(6, 'VinFast ', 7, 'pink', 'This is a car of South Korea', 'Solar cars', '', 'Haiphong', 'VinFast ', 145000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(7, 'SsangYong Motor', 45, 'black', 'This is a car of Seoul', 'Solar cars', '', 'SsangYong', 'Sora', 367000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(8, 'SsangYong Motor', 45, 'black', 'This is a car of Seoul', 'Solar cars', '', 'SsangYong', 'Sora', 367000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(9, 'Brabus', 7, 'blues', 'This is a car of Germany', 'Petrol engines', '', 'Bottrop', 'Brabus', 34500000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(10, 'Jetta ', 7, 'blues', 'This is a car of China', 'Solar cars', '', 'Jilin', 'Jetta  ', 398000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(11, 'MAZ', 45, 'lightgrey', 'This is a car of Belarus', 'Petrol engines', '', 'Minsk', 'Minsk Automobile Plant ', 123000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(12, 'SAIC Motor ', 7, 'pink', 'This is a car of China', 'Solar cars', '', 'Shanghai', 'SAIC Motor ', 145000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(13, 'MAN', 3, 'black', 'This is a car of  Germany', 'Solar cars', '', 'Munich', 'MAN', 367000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(14, 'VLF', 7, 'lightgrey', 'This is a car of United States', 'Petrol engines', '', 'America', 'VLF', 123000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(15, 'Vencer ', 7, 'pink', 'This is a car of Netherlands', 'Petrol engines', '', 'Vriezenveen', 'Vencer', 145000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(16, 'Volkswagen', 7, 'black', 'This is a car of Germany', 'Petrol engines', '', 'Wolfsburg', 'Volkswagen', 367000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(17, 'Toyota Motor', 7, 'lightgrey', 'This is a car of Japan', 'Petrol engines', '', 'Aichi', 'Toyota Motor', 123000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(18, 'Michelin ', 7, 'pink', 'This is a car of France', 'Solar cars', '', 'Clermont-Ferrand', 'Michelin', 145000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(19, 'Bridgestone', 3, 'black', 'This is a car of  Japan', 'Petrol engines', '', 'Tokyo', 'Bridgestone', 367000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(20, 'Mercedes-Benz', 7, 'lightgrey', 'This is a car of Germany', 'Petrol engines', '', 'Stuttgart', 'Mercedes-Benz', 123000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(21, 'Ford Motor ', 7, 'pink', 'This is a car of U.S', 'Petrol engines', '', 'Michigan', 'Ford Motor', 145000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(22, 'BMW ', 7, 'black', 'This is a car of Germany', 'Petrol engines', '', 'Munich', 'BMW ', 367000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(23, 'Honda  ', 7, 'white', 'This is a car of Japan', 'Petrol engines', '', 'Tokyo', 'Honda  ', 450000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(24, 'General Motors', 7, 'blues', 'This is a car of United States', 'Petrol engines', '', 'Michigan', 'General Motors', 34500000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(25, 'Hyundai Motor ', 7, 'blues', 'This is a car of South Korea', 'Solar cars', '', 'Seoul', 'Hyundai Motor  ', 398000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(26, 'Dongfeng Motor', 7, 'lightgrey', 'This is a car of China', 'Petrol engines', '', 'Hubei', 'Dongfeng Motor', 123000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(27, 'Nissan  ', 7, 'pink', 'This is a car of Japan', 'Solar cars', '', 'Yokohama', 'Nissan ', 145000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(28, 'Kia Motor', 3, 'black', 'This is a car of  South Korea', 'Petrol engines', '', 'Seoul', 'Kia Motor', 367000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(29, 'Renault', 7, 'lightgrey', 'This is a car of France', 'Petrol engines', '', 'Boulogne-Billancourt', 'Renault ', 123000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(30, 'Geely  ', 7, 'pink', 'This is a car of China', 'Petrol engines', '', 'Zhejiang', 'Geely ', 145000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(31, 'Tata ', 7, 'black', 'This is a car of India', 'Petrol engines', '', 'Maharashtra', 'Tata ', 367000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(32, 'Continental  ', 7, 'black', 'This is a car of Germany', 'Petrol engines', '', 'Hanover', 'Continental  ', 367000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(33, 'Goodyear  ', 7, 'black', 'This is a car of U.S', 'Petrol engines', '', 'Ohio', 'Goodyear  ', 367000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(34, 'Sumitomo  ', 7, 'black', 'This is a car of Japan', 'Petrol engines', '', 'Hyogo', 'Sumitomo  ', 367000000);
-INSERT INTO  public.vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
+INSERT INTO  vehicles (vehicle_id, brand, capacity, color, description, engine_type, image, location, name, pricing)
 VALUES(35, 'Hankook  ', 7, 'black', 'This is a car of South Korea', 'Petrol engines', '', 'Seoul', 'Hankook  ', 367000000);
 
-INSERT INTO public.hour (hour_id, hour)VALUES(1, 'AM') , (2, 'PM');
+INSERT INTO hour (hour_id, hour)VALUES(1, 'AM') , (2, 'PM');
 
-INSERT INTO public.vehicles_hour
+INSERT INTO vehicles_hour
 (status, hour_id, vehicle_id)
 VALUES(true, 1, 1) ,
       (true, 2, 1),
