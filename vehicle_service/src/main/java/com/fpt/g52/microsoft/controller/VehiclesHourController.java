43b3e392972802abb77bc4c0800d731bfc6c807f
tranquocnha <tranquocnha180800@gmail.com>
2023-05-11 09:35:48 +0700

    update crossorigin

--- vehicle_service/src/main/java/com/fpt/g52/microsoft/controller/VehiclesHourController.java
+++ vehicle_service/src/main/java/com/fpt/g52/microsoft/controller/VehiclesHourController.java
@@ -15,7 +15,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/v1/api/vehicles")
 public class VehiclesHourController {
