87c818018820185482546d79b160bc22ecc080ea
nhatnm <minhnhat6695@gmail.com>
2023-06-05 11:53:46 +0700

    add truncate table users

--- identity_service/src/main/resources/sql/data.sql
+++ identity_service/src/main/resources/sql/data.sql
@@ -1,3 +1,4 @@
+truncate users;
 INSERT INTO `users` (`roles`, `email`, `first_name`, `last_name`, `password`) VALUES
 ('ROLE_ADMIN', 'admin@fpt.com', 'Admin', 'Admin', '$2a$10$IBkyolQqVsOjTYa0Phro8efEuZvFAEUp94ZAH/Il8bTMzyiIYtEaS'),
 ('ROLE_USER', 'user@fpt.com', 'Uesr', 'User', '$2a$10$LQ3LcUEArqoiLLkJwwrEBeIfxMscIMWkKwEckpwvNvWsVvlG5Oy8C'),
